446471a187b8722a9912776b914c8b745ff39176
zongzhili <zongzhil@nycstudents.net>
2024-10-08 13:57:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ const products = [
   { name: "Chair", price: 250, category: "Furniture", rating: 4.0 },
   { name: "Desk", price: 450, category: "Furniture", rating: 4.8 },
 ];
+const updatedProducts = products.map((product) => ({
+  ...product,
+  price: (product.price * 1.1).toFixed(2),
+}));
+updatedProducts.forEach((product) => console.log(product.name, product.price));
+
+const categories = [];
+products.forEach((product) => {
+  if (!categories.includes(product.category)) {
+    categories.push(product.category);
+  }
+});
+console.log("Unique Categories:", categories);
+
+const electronics = products.filter(
+  (product) => product.category === "Electronics"
+);
+console.log("Electronics:", electronics);
+
+const expensiveProducts = products.filter((product) => product.price > 300);
+console.log("Expensive Products:", expensiveProducts);
+
+const goodProducts = products.filter((product) => product.rating >= 4.5);
+console.log("Good Products:", product.name);
